74a3c303a7e871a20524af1b1d98179e3a2316c1
Xalonious <74146476+xalonious@users.noreply.github.com>
2022-03-17 11:24:47 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express")
+
+const port = 2000;
+
+const app = express()
+
+app.get("/", (req, res) => {
+    res.status('200').sendFile(path.join(__dirname, "pages,", "dino.html"))
+})
+
+app.listen(port)
+
+console.log(`Listening at port ${port}`)
